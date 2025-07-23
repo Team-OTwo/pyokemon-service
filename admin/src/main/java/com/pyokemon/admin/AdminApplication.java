0e13a59e750a5613708ec136658551f40eb2012f
surnavy <113087821+surnavy@users.noreply.github.com>
2025-07-23 13:35:04 +0900

    [feature] scanBasePackages 설정 #4

--- admin/src/main/java/com/pyokemon/admin/AdminApplication.java
+++ admin/src/main/java/com/pyokemon/admin/AdminApplication.java
@@ -3,7 +3,7 @@ package com.pyokemon.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.pyokemon"})
 public class AdminApplication {
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
