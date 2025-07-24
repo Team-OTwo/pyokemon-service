f94a4a7bc899d87c929f43fe31b1f636d6464ff9
soobin70 <142298498+soobin70@users.noreply.github.com>
2025-07-24 17:09:22 +0900

    Update EventScheduleRepository.java

--- event/src/main/java/com/pyokemon/event/repository/EventScheduleRepository.java
+++ event/src/main/java/com/pyokemon/event/repository/EventScheduleRepository.java
@@ -1,6 +1,6 @@
 package com.pyokemon.event.repository;
 
-import com.pyokemon.event.entity.EventSchedule;
+import com.pyokemon.event.dto.EventItemResponseDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
