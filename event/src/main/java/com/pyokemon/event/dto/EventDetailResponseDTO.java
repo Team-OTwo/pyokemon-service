f5c27a41a9e2fe9e371e68ee3782ae785c20f5ff
sky121016 <sky121016@naver.com>
2025-07-24 13:40:44 +0900

    [Feature/PYOKEMON-29] 안쓰는 import 제거

--- event/src/main/java/com/pyokemon/event/dto/EventDetailResponseDTO.java
+++ event/src/main/java/com/pyokemon/event/dto/EventDetailResponseDTO.java
@@ -1,12 +1,6 @@
 package com.pyokemon.event.dto;
-
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.pyokemon.event.entity.Event;
-import com.pyokemon.event.entity.Event.EventStatus;
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-
 import java.time.LocalDateTime;
 
 @Data
