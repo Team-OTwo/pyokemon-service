d8ecc42020cd900bf331adb98ec725f125c8fa0e
danajlim <danalim0819@gmail.com>
2025-07-24 12:59:36 +0900

    Update EventRepository.java

--- event/src/main/java/com/pyokemon/event/repository/EventRepository.java
+++ event/src/main/java/com/pyokemon/event/repository/EventRepository.java
@@ -1,20 +1,15 @@
 package com.pyokemon.event.repository;
 
-import java.util.List;
-
+import com.pyokemon.event.entity.Event;
 import org.springframework.stereotype.Repository;
 
-import com.pyokemon.event.entity.Event;
+import java.util.List;
 
 @Repository
-public interface EventRepository {
-  List<Event> findByTenantId(Long tenantId);
-
-  List<Event> findByStatus(Event.EventStatus status);
-
-  List<Event> findByGenre(String genre);
-
-  List<Event> findByTitleContainingIgnoreCase(String title);
-
-  List<Event> findByAgeLimit(Long ageLimit);
+public interface EventRepository{
+    List<Event> findByTenantId(Long tenantId);
+    List<Event> findByStatus(Event.EventStatus status);
+    List<Event> findByGenre(String genre);
+    List<Event> findByTitleContainingIgnoreCase(String title);
+    List<Event> findByAgeLimit(Long ageLimit);
 }
