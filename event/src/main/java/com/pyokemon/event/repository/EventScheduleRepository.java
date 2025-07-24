dbdf8171a89655577d48988fa21938ef8fda309b
seoyun <chaeseoyoun8694@gmail.com>
2025-07-24 15:15:19 +0900

    [Fix] Repository의 Select문 Mapper로 이동 #9

--- event/src/main/java/com/pyokemon/event/repository/EventScheduleRepository.java
+++ event/src/main/java/com/pyokemon/event/repository/EventScheduleRepository.java
@@ -2,18 +2,10 @@ package com.pyokemon.event.repository;
 
 import com.pyokemon.event.entity.EventSchedule;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
 
-import java.util.List;
 import java.util.Optional;
 
 @Mapper
 public interface EventScheduleRepository {
-    @Select("SELECT event_schedule_id, event_id, venue_id, event_date, ticket_open_at, created_at, updated_at " +
-            "FROM tb_event_schedule " +
-            "WHERE event_schedule_id = #{id}")
-    Optional<EventSchedule> findById(@Param("id") Long id);
-
-    List<EventSchedule> findAll();
+    Optional<EventSchedule> findById(Long id);
 }
