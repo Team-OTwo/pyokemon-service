58ac318890ebf958f2a3949bd290521f146c4bec
soobin70 <142298498+soobin70@users.noreply.github.com>
2025-07-24 17:01:46 +0900

    Update EventController.java

--- event/src/main/java/com/pyokemon/event/controller/EventController.java
+++ event/src/main/java/com/pyokemon/event/controller/EventController.java
@@ -13,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/events")
 public class EventController {
 
     private final EventScheduleService eventScheduleService;
