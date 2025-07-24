2014e94eab09bed01e42a91d28972d694abf0057
soobin70 <142298498+soobin70@users.noreply.github.com>
2025-07-24 14:07:35 +0900

    [fix/PYOKEMON-21]Update EventScheduleService.

--- event/src/main/java/com/pyokemon/event/service/EventScheduleService.java
+++ event/src/main/java/com/pyokemon/event/service/EventScheduleService.java
@@ -8,10 +8,10 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class EventScheduleService {
 
-    @Autowired
-    private EventScheduleRepository eventScheduleRepository;
+    private final EventScheduleRepository eventScheduleRepository;
     public List<EventSchedule> getTodayOpenedTickets() {
        return eventScheduleRepository.selectTodayOpenedTickets();
     }
