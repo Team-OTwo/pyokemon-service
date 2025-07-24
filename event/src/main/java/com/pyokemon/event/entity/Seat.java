3c776e1e8b998268d79332e4ab75f752ecae0376
danajlim <danalim0819@gmail.com>
2025-07-24 12:58:09 +0900

    Update Seat.java

--- event/src/main/java/com/pyokemon/event/entity/Seat.java
+++ event/src/main/java/com/pyokemon/event/entity/Seat.java
@@ -1,8 +1,8 @@
 package com.pyokemon.event.entity;
 
-import java.time.LocalDateTime;
-
 import lombok.*;
+
+import java.time.LocalDateTime;
 
 @Getter
 @Setter
@@ -10,12 +10,12 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Seat {
-  private Long seatId;
-  private Long venueId;
-  private Long seatClassId;
-  private Long floor;
-  private Long row;
-  private Long col;
-  private LocalDateTime createdAt;
-  private LocalDateTime updatedAt;
+    private Long seatId;
+    private Long venueId;
+    private Long seatClassId;
+    private Long floor;
+    private Long row;
+    private Long col;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
 }
