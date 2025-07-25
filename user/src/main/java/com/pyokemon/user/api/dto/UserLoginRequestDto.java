4b6da1e0e5bdbaf94948502e0b84eaf1adad6155
surnavy <113087821+surnavy@users.noreply.github.com>
2025-07-25 13:34:03 +0900

    [fix] 기능별 Dto 생성 #15

---
+++ user/src/main/java/com/pyokemon/user/api/dto/UserLoginRequestDto.java
@@ -0,0 +1,20 @@
+package com.pyokemon.user.api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginRequestDto {
+
+    @NotBlank(message = "이메일은 필수입니다.")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다.")
+    private String password;
+}
