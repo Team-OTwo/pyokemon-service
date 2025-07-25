eebc2c1bc5944542650bcfb7b335fd24d87c12ea
dldh1218 <dldh1218@gmail.com>
2025-07-25 16:53:12 +0900

    [fix/PYOKEMON-14] merge conflict 수정

--- event/src/main/java/com/pyokemon/event/controller/EventController.java
+++ event/src/main/java/com/pyokemon/event/controller/EventController.java
@@ -2,43 +2,58 @@ package com.pyokemon.event.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.pyokemon.common.dto.ResponseDto;
 import com.pyokemon.event.dto.EventDetailResponseDTO;
 import com.pyokemon.event.dto.EventItemResponseDTO;
+import com.pyokemon.event.dto.EventRegisterDto;
+import com.pyokemon.event.dto.EventResponseDto;
 import com.pyokemon.event.entity.Event;
 import com.pyokemon.event.service.EventScheduleService;
 import com.pyokemon.event.service.EventService;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/events")
 @RequiredArgsConstructor
 public class EventController {
-  private final EventService eventService;
-  private final EventScheduleService eventScheduleService;
+    private final EventService eventService;
+    private final EventScheduleService eventScheduleService;
 
-  // 오늘 오픈 티켓
-  @GetMapping("/open-today")
-  public List<EventItemResponseDTO> getOpenTicketsToday() {
-    return eventScheduleService.getTodayOpenedTickets();
-  }
+    //이벤트 등록
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public ResponseDto<EventResponseDto> registerEvent(@Valid @RequestBody EventRegisterDto eventRegisterDto) {
+        EventResponseDto registeredEvent = eventService.registerEvent(eventRegisterDto);
+        return ResponseDto.success(registeredEvent, "Event registered successfully");
+    }
 
-  // 오픈 예정 티켓
-  @GetMapping("/to-be-opend")
-  public List<EventItemResponseDTO> getOpenTicketsToBeOpened() {
-    return eventScheduleService.getTicketsToBeOpened();
-  }
+    // 오늘 오픈 티켓
+    @GetMapping("/open-today")
+    public List<EventItemResponseDTO> getOpenTicketsToday() {
+        return eventScheduleService.getTodayOpenedTickets();
+    }
 
-  @GetMapping("/{eventId}")
-  public ResponseEntity<EventDetailResponseDTO> getEventDetail(@PathVariable Long eventId) {
-    EventDetailResponseDTO dto = eventService.getEventDetailByEventId(eventId);
-    return ResponseEntity.ok(dto);
-  }
+    // 오픈 예정 티켓
+    @GetMapping("/to-be-opened")
+    public List<EventItemResponseDTO> getOpenTicketsToBeOpened() {
+        return eventScheduleService.getTicketsToBeOpened();
+    }
 
+    @GetMapping("/{eventId}")
+    public ResponseEntity<EventDetailResponseDTO> getEventDetail(@PathVariable Long eventId) {
+        EventDetailResponseDTO dto = eventService.getEventDetailByEventId(eventId);
+        return ResponseEntity.ok(dto);
+    }
 }
--- event/src/main/java/com/pyokemon/event/service/EventScheduleService.java
+++ event/src/main/java/com/pyokemon/event/service/EventScheduleService.java
@@ -2,23 +2,24 @@ package com.pyokemon.event.service;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.pyokemon.event.dto.EventItemResponseDTO;
 import com.pyokemon.event.repository.EventScheduleRepository;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
 @RequiredArgsConstructor
 public class EventScheduleService {
 
-  private final EventScheduleRepository eventScheduleRepository;
+    private final EventScheduleRepository eventScheduleRepository;
 
-  public List<EventItemResponseDTO> getTodayOpenedTickets() {
-    return eventScheduleRepository.selectTodayOpenedTickets();
-  }
+    public List<EventItemResponseDTO> getTodayOpenedTickets() {
+        return eventScheduleRepository.selectTodayOpenedTickets();
+    }
 
-  public List<EventItemResponseDTO> getTicketsToBeOpened() {
-    return eventScheduleRepository.selectTicketsToBeOpened();
-  }
+    public List<EventItemResponseDTO> getTicketsToBeOpened() {
+        return eventScheduleRepository.selectTicketsToBeOpened();
+    }
 }
