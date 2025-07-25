b511b01107fcaa9b78d7f8abe8c91b3cf0cd5e38
dldh1218 <dldh1218@gmail.com>
2025-07-25 13:23:47 +0900

    [fix/PYOKEMON-14] EventService 단일 클래스로 수정

--- event/src/main/java/com/pyokemon/event/repository/VenueRepository.java
+++ event/src/main/java/com/pyokemon/event/repository/VenueRepository.java
@@ -7,6 +7,5 @@ import com.pyokemon.event.entity.Venue;
 
 @Mapper
 public interface VenueRepository {
-  @Select("SELECT venue_id, venue_name, city, street, zipcode, created_at, updated_at FROM tb_venue WHERE venue_id = #{venueId}")
-  Optional<Venue> findById(Long venueId);
+    Optional<Venue> findById(Long venueId);
 }
