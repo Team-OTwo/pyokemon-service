947e62729beb609dbd5479628a22efa0792b797e
danajlim <danalim0819@gmail.com>
2025-07-30 10:13:26 +0900

    fix: mybatis mapperscan 추가

--- account/src/test/java/com/pyokemon/account/tenant/service/TenantServiceTest.java
+++ account/src/test/java/com/pyokemon/account/tenant/service/TenantServiceTest.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import com.pyokemon.account.auth.entity.AccountStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
