0ace6522644f81f3fa0fe23e7b9dff032af1ae1e
soobin70 <142298498+soobin70@users.noreply.github.com>
2025-07-30 10:02:15 +0900

    [fix]  Security 기본 설정 비활성화

--- event/src/main/java/com/pyokemon/event/EventApplication.java
+++ event/src/main/java/com/pyokemon/event/EventApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.pyokemon"})
+@SpringBootApplication(scanBasePackages = {"com.pyokemon"},exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
 @MapperScan("com.pyokemon.event.repository")
 public class EventApplication {
   public static void main(String[] args) {
