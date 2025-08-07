47e18d2b96a10ae1838ae7d34abbdf64ef2929c8
surnavy <113087821+surnavy@users.noreply.github.com>
2025-08-07 18:03:35 +0900

    [fix] SecurityConfig common으로 이동 #68

--- common/src/main/java/com/pyokemon/common/config/SecurityConfig.java
+++ common/src/main/java/com/pyokemon/common/config/SecurityConfig.java
@@ -1,15 +1,12 @@
-package com.pyokemon.account.common.config;
-
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+package com.pyokemon.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,10 +17,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 //import com.pyokemon.account.auth.secret.jwt.JwtAuthenticationFilter;
-import com.pyokemon.common.dto.ResponseDto;
-import com.pyokemon.common.exception.code.AccountErrorCodes;
 
-import lombok.RequiredArgsConstructor;
 
 @Configuration
 //@EnableWebSecurity
@@ -32,31 +26,21 @@ public class SecurityConfig {
 
   private final ObjectMapper objectMapper = new ObjectMapper();
 
-//  @Bean
-//  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//    http.csrf(csrf -> csrf.disable())
-//        .sessionManagement(
-//            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//        .authorizeHttpRequests(authz -> authz
-//            // 공개 API
-//            .requestMatchers("/api/login", "/api/users", "/api/tenants", "/api/login/app").permitAll()
-//            // 관리자 전용 API
-//            .requestMatchers("/api/admin/**").hasRole("ADMIN")
-//            // 테넌트 API (테넌트 본인 + 관리자)
-//            .requestMatchers("/api/tenants/profile/**").hasRole("TENANT")
-//            .requestMatchers("/api/tenants/**").hasAnyRole("TENANT", "ADMIN")
-//            // 사용자 API
-//            .requestMatchers("/api/users/profile/**").hasRole("USER")
-//            .requestMatchers("/api/users/**").hasRole("USER")
-//            // 기타 모든 요청은 인증 필요
-//            .anyRequest().authenticated())
+  @Bean
+  public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+            .cors(AbstractHttpConfigurer::disable)
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authz -> authz
+            .anyRequest().permitAll());
 //        .addFilterBefore(new JwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
 //        .exceptionHandling(exceptionHandling -> exceptionHandling
 //            .authenticationEntryPoint(customAuthenticationEntryPoint())
 //            .accessDeniedHandler(customAccessDeniedHandler()));
-//
-//    return http.build();
-//  }
+
+    return http.build();
+  }
 //
 //  @Bean
 //  public AuthenticationEntryPoint customAuthenticationEntryPoint() {
