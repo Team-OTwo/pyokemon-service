ec659016da89bc52f9115afe91a206c819f08e51
dldh1218 <dldh1218@gmail.com>
2025-08-08 16:57:10 +0900

    Revert "Merge branch 'develop' of https://github.com/Team-OTwo/pyokemon-service into feature/PYOKEMON-113-메인-페이지-월간-이벤트-조회-api"
    This reverts commit 59fd0762d9d5b7bcfbafd336c4af662b672eeab9, reversing
    changes made to 74f34d82423280214d983a7b95b548d3aa933d0e.

--- common/src/main/java/com/pyokemon/common/exception/code/DidErrorCodes.java
+++
@@ -1,7 +0,0 @@
-package com.pyokemon.common.exception.code;
-
-public final class DidErrorCodes {
-    // 지갑 관련 에러.
-    public static final String WALLET_ALREADY_EXISTS = "WALLET_ALREADY_EXISTS";
-    public static final String WALLET_CREATION_FAILED = "WALLET_CREATION_FAILED";
-}
