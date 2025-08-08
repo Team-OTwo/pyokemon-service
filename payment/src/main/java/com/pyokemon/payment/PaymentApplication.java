76b64b017877efa255d84b6cd5462188eec5231e
soobin70 <142298498+soobin70@users.noreply.github.com>
2025-08-08 16:07:07 +0900

    [fix] security 적용 안되게

--- payment/src/main/java/com/pyokemon/payment/PaymentApplication.java
+++ payment/src/main/java/com/pyokemon/payment/PaymentApplication.java
@@ -4,7 +4,10 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.pyokemon"})
+@SpringBootApplication(scanBasePackages = {"com.pyokemon"},
+        exclude = {
+                org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class}
+)
 @MapperScan("com.pyokemon.payment.repository")
 public class PaymentApplication {
   public static void main(String[] args) {
