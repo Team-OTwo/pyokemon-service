1bdc1b25bb0c7f3d6390271e5d9c0e8683f66df1
dldh1218 <dldh1218@gmail.com>
2025-08-12 09:16:58 +0900

    fix/account 서비스 테넌트 계정 로그인 수정

--- account/src/main/java/com/pyokemon/account/auth/service/AccountService.java
+++ account/src/main/java/com/pyokemon/account/auth/service/AccountService.java
@@ -43,7 +43,7 @@ public class AccountService {
     private final RedisTemplate<String, String> redisTemplate;
 
     @Transactional
-    public LoginResponseDto login(LoginRequestDto request) {
+    public LoginResponseDto login(LoginRequestDto request, String role) {
         log.info("로그인 시도: {}", request.getLoginId());
 
         // 계정 조회
@@ -67,6 +67,7 @@ public class AccountService {
         String refreshToken =
                 tokenGenerator.generateRefreshToken(account.getAccountId(), account.getRole());
 
+        if (role.equals("USER")) {
         log.info("로그인 성공: {} (역할: {})", request.getLoginId(), account.getRole());
 
         Optional<User> userOpt = userRepository.findByAccountId(account.getAccountId());
@@ -85,6 +86,14 @@ public class AccountService {
                 .accountId(account.getAccountId())
                 .isVerified(user.getIsVerified())
                 .build();
+        } else {
+            return LoginResponseDto.builder()
+                    .accessToken(accessToken)
+                    .refreshToken(refreshToken)
+                    .role(account.getRole())
+                    .accountId(account.getAccountId())
+                    .build();
+        }
     }
 
     @Transactional
