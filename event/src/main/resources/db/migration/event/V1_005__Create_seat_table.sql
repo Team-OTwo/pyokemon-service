1295b5199a573007285f699a8aeb7b78c86a1374
dldh1218 <dldh1218@gmail.com>
2025-08-12 13:52:36 +0900

    fix/db 마이그레이션 수정

--- event/src/main/resources/db/migration/event/V1_005__Create_seat_table.sql
+++ event/src/main/resources/db/migration/event/V1_005__Create_seat_table.sql
@@ -9,6 +9,3 @@ CREATE TABLE tb_seat (
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-CREATE INDEX idx_seat_venue_id ON tb_seat(venue_id);
-CREATE INDEX idx_seat_class_id ON tb_seat(seat_class_id);
