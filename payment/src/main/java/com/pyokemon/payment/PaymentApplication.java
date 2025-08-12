b9f828716cb21669c4aa839110aa3c5632b4d909
soobin70 <142298498+soobin70@users.noreply.github.com>
2025-08-12 22:13:09 +0900

    [fix] yml 수정

--- payment/src/main/java/com/pyokemon/payment/PaymentApplication.java
+++ payment/src/main/java/com/pyokemon/payment/PaymentApplication.java
@@ -3,10 +3,9 @@ package com.pyokemon.payment;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = {"com.pyokemon"},
-    exclude = {
-        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@SpringBootApplication(scanBasePackages = {"com.pyokemon"})
 @MapperScan("com.pyokemon.payment.repository")
 public class PaymentApplication {
   public static void main(String[] args) {
