281a8feaf427238d10164abb4f12b18c678bbc1a
seoyoun8694 <chaeseoyoun8694@gmail.com>
2025-08-13 12:54:10 +0900

    [feature] dto #89

---
+++ booking/src/main/java/com/pyokemon/booking/dto/request/ValidBookingRequest.java
@@ -0,0 +1,19 @@
+package com.pyokemon.booking.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidBookingRequest {
+    private Long userId;
+    private List<Long> bookings;
+}
---
+++ booking/src/main/java/com/pyokemon/booking/dto/response/ValidBookingResponse.java
@@ -0,0 +1,18 @@
+package com.pyokemon.booking.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidBookingResponse {
+    private List<ValidBookingDetail> bookings;
+}
