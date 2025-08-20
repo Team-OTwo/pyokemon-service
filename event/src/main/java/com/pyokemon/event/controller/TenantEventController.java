3c7e689ce4003f1a39f86cf68565429a42659535
sky121016 <sky121016@naver.com>
2025-08-20 13:16:04 +0900

    [Feature/#102] 테넌트 공연 조회 limit, 기간 지남 수정

--- event/src/main/java/com/pyokemon/event/controller/TenantEventController.java
+++ event/src/main/java/com/pyokemon/event/controller/TenantEventController.java
@@ -88,7 +88,7 @@ public class TenantEventController {
     // 앱 커서기반 공연 조회
     @GetMapping("/app")
     public ResponseDto<TenantEventListResponseDtoForApp> getEventListForApp(@RequestHeader(value = "X-Auth-AccountId") Long accountId, @RequestParam(required = false) LocalDateTime cursorDate, @RequestParam(required = false) Long cursorId,
-                                                                            @RequestParam(defaultValue = "2") int limit) {
+                                                                            @RequestParam(defaultValue = "8") int limit) {
         List<TenantEventDetailDtoForApp> events = tenantEventService.getEventListForApp(accountId, cursorDate, cursorId, limit + 1);
 
         TenantEventListResponseDtoForApp response = new TenantEventListResponseDtoForApp();
