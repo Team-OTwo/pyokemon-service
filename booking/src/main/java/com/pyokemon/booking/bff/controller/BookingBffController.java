eb894873c70555dd940438eac332cc1891cf88ce
sky121016 <sky121016@naver.com>
2025-08-21 10:12:43 +0900

    [Feature] 예매 내역 조회 최신순, 페이징 구현

--- booking/src/main/java/com/pyokemon/booking/bff/controller/BookingBffController.java
+++ booking/src/main/java/com/pyokemon/booking/bff/controller/BookingBffController.java
@@ -1,11 +1,13 @@
 package com.pyokemon.booking.bff.controller;
 
-import com.pyokemon.booking.bff.dto.BookingDto;
-import com.pyokemon.booking.bff.service.BookingBffService;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import com.pyokemon.booking.bff.dto.BookingDto;
+import com.pyokemon.booking.bff.service.BookingBffService;
+
+import lombok.RequiredArgsConstructor;
 
 
 @RestController
@@ -13,27 +15,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class BookingBffController {
 
-    private final BookingBffService bookingBffService;
+  private final BookingBffService bookingBffService;
 
-    @GetMapping("/event-schedules/{eventScheduleId}/bookings")
-    public List<BookingDto> getEventScheduleBookings(@PathVariable Long eventScheduleId) {
-        return bookingBffService.getEventScheduleBookings(eventScheduleId);
-    }
+  @GetMapping("/event-schedules/{eventScheduleId}/bookings")
+  public List<BookingDto> getEventScheduleBookings(@PathVariable Long eventScheduleId) {
+    return bookingBffService.getEventScheduleBookings(eventScheduleId);
+  }
 
-    @GetMapping("/accounts/{accountId}/bookings")
-    public List<BookingDto> getAccountBookings(@PathVariable Long accountId) {
-        return bookingBffService.getAccountIdBookings(accountId);
-    }
+  @GetMapping("/accounts/{accountId}/bookings")
+  public List<BookingDto> getAccountBookings(@PathVariable Long accountId) {
+    return bookingBffService.getAccountIdBookings(accountId);
+  }
 
-    @GetMapping("/accounts/{accountId}/bookings/order")
-    public List<BookingDto> getAccountBookingsOrderByDate(@PathVariable Long accountId, @RequestParam(required = false, defaultValue = "0") Integer page,
-                                                          @RequestParam(required = false, defaultValue = "10") Integer size) {
-        return bookingBffService.getAccountIdBookingsOrderByDate(accountId, page, size);
-    }
+  @GetMapping("/accounts/{accountId}/bookings/order")
+  public List<BookingDto> getAccountBookingsOrderByDate(@PathVariable Long accountId,
+      @RequestParam(required = false, defaultValue = "0") Integer page,
+      @RequestParam(required = false, defaultValue = "10") Integer size) {
+    return bookingBffService.getAccountIdBookingsOrderByDate(accountId, page, size);
+  }
 
-    @GetMapping("/bff/{bookingId}")
-    public BookingDto getBooking(@PathVariable Long bookingId) {
-        return bookingBffService.getBooking(bookingId);
-    }
+  @GetMapping("/bff/{bookingId}")
+  public BookingDto getBooking(@PathVariable Long bookingId) {
+    return bookingBffService.getBooking(bookingId);
+  }
 
 }
--- booking/src/main/java/com/pyokemon/booking/bff/service/BookingBffService.java
+++ booking/src/main/java/com/pyokemon/booking/bff/service/BookingBffService.java
@@ -1,14 +1,16 @@
 package com.pyokemon.booking.bff.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
 import com.pyokemon.booking.bff.dto.BookingDto;
 import com.pyokemon.booking.bff.repository.BookingBffRepository;
 import com.pyokemon.booking.entity.Booking;
 import com.pyokemon.common.exception.BusinessException;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-import java.util.List;
-import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
@@ -23,40 +25,39 @@ public class BookingBffService {
       return null;
     }
 
-    return bookings.stream()
-            .map(this::toDto)      // Booking -> BookingDto
-            .toList(); // Java 16+면 이거 사용 가능
+    return bookings.stream().map(this::toDto) // Booking -> BookingDto
+        .toList(); // Java 16+면 이거 사용 가능
   }
 
-  public List<BookingDto> getAccountIdBookings(Long accountId){
+  public List<BookingDto> getAccountIdBookings(Long accountId) {
     List<Booking> bookings = bookingBffRepository.findByAccountId(accountId);
 
     if (bookings.isEmpty()) {
       return null;
     }
 
-    return bookings.stream()
-            .map(this::toDto)      // Booking -> BookingDto
-            .toList();
+    return bookings.stream().map(this::toDto) // Booking -> BookingDto
+        .toList();
   }
 
-  public List<BookingDto> getAccountIdBookingsOrderByDate(Long accountId, Integer page, Integer size){
-    List<Booking> bookings = bookingBffRepository.findByAccountIdOrderByDate(accountId, page*size, size);
+  public List<BookingDto> getAccountIdBookingsOrderByDate(Long accountId, Integer page,
+      Integer size) {
+    List<Booking> bookings =
+        bookingBffRepository.findByAccountIdOrderByDate(accountId, page * size, size);
 
     if (bookings.isEmpty()) {
       return null;
     }
 
-    return bookings.stream()
-            .map(this::toDto)      // Booking -> BookingDto
-            .toList();
+    return bookings.stream().map(this::toDto) // Booking -> BookingDto
+        .toList();
   }
 
-  public BookingDto getBooking(Long bookingId){
+  public BookingDto getBooking(Long bookingId) {
     Optional<Booking> bookingOpt = bookingBffRepository.findByBookingId(bookingId);
 
-    if (bookingOpt.isEmpty()){
-      throw new BusinessException("해당 예약은 존재하지 않습니다.","BOOKING_NOT_FOUND");
+    if (bookingOpt.isEmpty()) {
+      throw new BusinessException("해당 예약은 존재하지 않습니다.", "BOOKING_NOT_FOUND");
     }
 
     Booking booking = bookingOpt.get();
@@ -65,14 +66,8 @@ public class BookingBffService {
   }
 
   private BookingDto toDto(Booking b) {
-    return BookingDto.builder()
-            .bookingId(b.getBookingId())
-            .eventScheduleId(b.getEventScheduleId())
-            .seatId(b.getSeatId())
-            .accountId(b.getAccountId())
-            .paymentId(b.getPaymentId())
-            .status(b.getStatus())
-            .updatedAt(b.getUpdatedAt())
-            .build();
+    return BookingDto.builder().bookingId(b.getBookingId()).eventScheduleId(b.getEventScheduleId())
+        .seatId(b.getSeatId()).accountId(b.getAccountId()).paymentId(b.getPaymentId())
+        .status(b.getStatus()).updatedAt(b.getUpdatedAt()).build();
   }
 }
