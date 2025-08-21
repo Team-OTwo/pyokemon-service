89ca834a76639eca71ca6090e1a18d6ecd1d53e7
soobin70 <142298498+soobin70@users.noreply.github.com>
2025-08-21 13:30:42 +0900

    feature/event kafka producer

---
+++ event/src/main/java/com/pyokemon/event/dto/kafka/EventKafkaDto.java
@@ -0,0 +1,16 @@
+package com.pyokemon.event.dto.kafka;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EventKafkaDto {
+    private Long eventScheduleId;
+    private String status;;
+}
