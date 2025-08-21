4eb2502714b824952fdd696acc034b88b9753626
surnavy <113087821+surnavy@users.noreply.github.com>
2025-08-21 16:50:43 +0900

    [fix] is_login Default 값 True로 설정

--- account/src/main/resources/db/migration/account/V1_005__Create_user_device_table.sql
+++ account/src/main/resources/db/migration/account/V1_005__Create_user_device_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE tb_user_device (
     device_number VARCHAR(50) NOT NULL,
     fcm_token TEXT,
     os_type ENUM('ANDROID', 'IOS') NOT NULL,
-    is_login BOOLEAN,
+    is_login BOOLEAN DEFAULT TRUE,
     is_valid BOOLEAN DEFAULT TRUE,
     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
