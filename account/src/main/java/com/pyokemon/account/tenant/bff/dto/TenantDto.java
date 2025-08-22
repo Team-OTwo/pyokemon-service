89ca2ed5bbfbf62708f9a3234e697a82f7215fce
Nebulaesthetic <163494134+Nebulaesthetic@users.noreply.github.com>
2025-08-22 16:38:10 +0900

    [feature] TenantDto추가

---
+++ account/src/main/java/com/pyokemon/account/tenant/bff/dto/TenantDto.java
@@ -0,0 +1,22 @@
+package com.pyokemon.account.tenant.bff.dto;
+
+import com.pyokemon.account.tenant.entity.Tenant;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TenantDto {
+
+    private Long tenantId;
+    private String name;
+
+    public static TenantDto from(Tenant tenant) {
+        return TenantDto.builder()
+                .tenantId(tenant.getTenantId())
+                .name(tenant.getName())
+                .build();
+    }
+}
