dca19d75a5e779555267a10615be41868c4078c5
Nebulaesthetic <163494134+Nebulaesthetic@users.noreply.github.com>
2025-08-27 09:38:21 +0900

    [feature] TotalRevenueResponseDto 작성 #157

---
+++ payment/src/main/java/com/pyokemon/payment/bff/dto/TotalRevenueResponseDto.java
@@ -0,0 +1,12 @@
+package com.pyokemon.payment.bff.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TotalRevenueResponseDto {
+    private Long totalRevenue;
+}
