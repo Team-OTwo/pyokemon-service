016b4b85f858be91114f055611d085ce49d25d03
Nebulaesthetic <163494134+Nebulaesthetic@users.noreply.github.com>
2025-08-27 09:44:36 +0900

    [feature] tenant web 최근공연현황 조회관련 event Endpoint 작성 #157

--- event/src/main/java/com/pyokemon/event/bff/controller/BffEventController.java
+++ event/src/main/java/com/pyokemon/event/bff/controller/BffEventController.java
@@ -2,6 +2,7 @@ package com.pyokemon.event.bff.controller;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.pyokemon.common.dto.IdsRequest;
@@ -73,5 +74,28 @@ public class BffEventController {
     return bffEventService.getVenues(request.getIds());
   }
 
+  @PostMapping("events/schedules/details/_batch")
+  public List<BffScheduleDetailDto> getScheduleDetails(@RequestBody IdsRequest request) {
+    return bffEventService.getScheduleDetailsByIds(request.getIds());
+  }
 
+  @GetMapping("/events/tenant/summary/count")
+  public ResponseEntity<ActiveEventCountResponseDto> getActiveEventCount(
+          @RequestHeader("x-auth-accountId") Long tenantId,
+          @RequestParam("year") int year,
+          @RequestParam("month") int month) {
+
+    ActiveEventCountResponseDto response = bffEventService.getActiveEventCount(tenantId, year, month);
+    return ResponseEntity.ok(response);
+  }
+
+  @GetMapping("events/tenant/schedules-by-tenant")
+  public ResponseEntity<ScheduleIdsResponseDto> getScheduleIdsByTenant(
+          @RequestHeader("x-auth-accountId") Long tenantId,
+          @RequestParam("year") int year,
+          @RequestParam("month") int month) {
+
+    ScheduleIdsResponseDto responseDto = bffEventService.getScheduleIdsByTenant(tenantId, year, month);
+    return ResponseEntity.ok(responseDto);
+  }
 }
