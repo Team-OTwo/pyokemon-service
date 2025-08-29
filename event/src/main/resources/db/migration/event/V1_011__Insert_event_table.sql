986c40f6721d177ef2fdae895e07cec6b6e41148
seoyun <chaeseoyoun8694@gmail.com>
2025-08-29 13:14:01 +0900

    [feature] insert sql 추가

--- event/src/main/resources/db/migration/event/V1_011__Insert_event_table.sql
+++ event/src/main/resources/db/migration/event/V1_011__Insert_event_table.sql
@@ -28,4 +28,32 @@ VALUES
      '전시회',
      'https://ticketimage.interpark.com/Play/image/large/25/25011411_p.gif',
      'APPROVED',
+     NOW(), NOW()),
+
+   (5, 5, '뮤지컬 〈맘마미아!〉', 12,
+     '뮤지컬 〈맘마미아!〉뮤지컬 설명',
+     '뮤지컬',
+     'https://ticketimage.interpark.com/Play/image/large/L0/L0000123_p.gif',
+     'APPROVED',
+     NOW(), NOW()),
+
+   (6, 6, '［2025 전시지원］ 워너 브롱크호스트', 12,
+     '［2025 전시지원］ 워너 브롱크호스트 전시회 설명',
+     '전시회',
+     'https://ticketimage.interpark.com/Play/image/large/25/25011387_p.gif',
+     'APPROVED',
+     NOW(), NOW()),
+
+   (7, 7, '잉글리시 콘서트 〈헨델, 리날도〉', 12,
+     '잉글리시 콘서트 〈헨델, 리날도〉클래식 설명',
+     '클래식',
+     'https://ticketimage.interpark.com/Play/image/large/25/25012245_p.gif',
+     'APPROVED',
+     NOW(), NOW()),
+
+   (8, 8, '뮤지컬 〈쉐도우〉', 12,
+     '뮤지컬 〈쉐도우> 뮤지컬 설명',
+     '뮤지컬',
+     'https://ticketimage.interpark.com/Play/image/large/25/25009941_p.gif',
+     'APPROVED',
      NOW(), NOW());
